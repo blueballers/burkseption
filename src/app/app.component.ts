1fa38d8039741fa2c11a7e791d39f0aff730c8a9
Mikk Visnap <mikk.visnap@gmail.com>
2018-12-10 22:42:48 +0200

    add firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+// RXJS
+import { Observable } from 'rxjs';
+
+// FIREBASE
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Burger } from './burger/burger.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +14,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'burkseption';
+
+  items: Observable<Burger[]>;
+
+  constructor(db: AngularFirestore) {
+    this.items = db.collection<Burger>('items').valueChanges();
+  }
+
 }
